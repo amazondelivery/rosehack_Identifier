c8b75ec869185fec287ed33bca74d1180c65d473
amazondelivery <mr.vikram.balaji@gmail.com>
2022-01-15 12:01:26 -0800

    Initial Commit

---
+++ rosehack_Identifier/rosehack_IdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  rosehack_IdentifierApp.swift
+//  rosehack_Identifier
+//
+//  Created by Vikram B on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct rosehack_IdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
